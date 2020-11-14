909aff5b3bed045db3ca989f27063228e210917f
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-14 13:01:08 +0000

    cleanup

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -4,15 +4,11 @@ import { Link } from 'react-router-dom';
 const SearchResult = ({ matches, term }) => {
 	if (term !== '') {
 		const renderedList = matches.map((match) => {
-			const regexHi = new RegExp(term, 'gi');
-			const matchTitle = match.title.replace(regexHi, term);
-			const matchDescription = match.description.replace(regexHi, term);
 			return (
 				<Link key={match.id} to={`/streams/${match.id}`}>
 					<li>
 						<span className="name">
-							<span className="hl">{matchTitle}</span> ,{' '}
-							<span className="hl">{matchDescription}</span>
+							<span>{match.title}</span> , <span>{match.description}</span>
 						</span>
 					</li>
 				</Link>
