a9f05f58d06d87b371e9ea36685944006d3f1ac2
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-14 15:10:07 +0000

    highlight matching letters in serach

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -4,11 +4,38 @@ import { Link } from 'react-router-dom';
 const SearchResult = ({ matches, term }) => {
 	if (term !== '') {
 		const renderedList = matches.map((match) => {
+			const regexHi = new RegExp(term, 'gi');
+
+			const title = (
+				<span>
+					{match.title.split(regexHi).map((char, index) => (
+						<>
+							{char}
+							{index !== match.title.split(regexHi).length - 1 && (
+								<span className="hl">{term}</span>
+							)}
+						</>
+					))}
+				</span>
+			);
+			const description = (
+				<span>
+					{match.description.split(regexHi).map((char, index) => (
+						<>
+							{char}
+							{index !== match.description.split(regexHi).length - 1 && (
+								<span className="hl">{term}</span>
+							)}
+						</>
+					))}
+				</span>
+			);
+
 			return (
 				<Link key={match.id} to={`/streams/${match.id}`}>
 					<li>
 						<span className="name">
-							<span>{match.title}</span> , <span>{match.description}</span>
+							{title},{description}{' '}
 						</span>
 					</li>
 				</Link>
